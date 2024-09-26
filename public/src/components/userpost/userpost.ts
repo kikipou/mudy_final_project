1807bbd25a5709e4b690cdaa62d44f27d4e052fc
LauraBonillaa <158177899+LauraBonillaa@users.noreply.github.com>
2024-09-26 17:54:51 -0500

    style: se le agregan estilos a la informacion de perfil en los posts

--- public/src/components/userpost/userpost.ts
+++ public/src/components/userpost/userpost.ts
@@ -38,10 +38,17 @@ class ArtistPost extends HTMLElement {
 
             this.shadowRoot.innerHTML = `
               <link rel="stylesheet" href="../public/src/components/userpost/userpost.css">
+              
                 <div class="post">
                 <div id=perfilinfo>
                 <img class="imgperfil" src="${profile}" alt="${artistName}">
-                <p>Uploaded: ${songTime} hours ago</p> <!-- Tiempo desde que se subió la canción -->
+                <div class="encabezado">
+               <p>${artistName}</p>
+               
+               <p>-</p>
+               
+                <p >${songTime}h ago</p> 
+                </div>
                  </div>
                     <div id="photo">
 
@@ -51,11 +58,11 @@ class ArtistPost extends HTMLElement {
                    
                     <div class="info">
                         <div class="titulo">
-                             <h1>${songName}</h1>
+                             <h2>${songName}</h2>
                         </div>
                         <div class="subtitulo">
                             <p>By ${artistName}</p>
-                            <p>Uploaded: ${songTime} hours ago</p> <!-- Tiempo desde que se subió la canción -->
+                          
                             <button class="heart-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
                                     <path class="heart-outline" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="none" stroke="#ff9da6" stroke-width="2"/>
