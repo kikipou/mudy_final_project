2c647de07fccd4875799175a6bd7504ba2d0475e
kikipou <kta2122006@gmail.com>
2024-09-27 17:36:02 -0500

    feat: header responsive

--- public/src/indexAbuelo.ts
+++ public/src/indexAbuelo.ts
@@ -46,7 +46,9 @@ class AppContainer extends HTMLElement {
                         profile="https://i.pinimg.com/564x/73/c1/b4/73c1b4a272425b7295eec9b0f874b8ee.jpg"
                         search="Search"
                         ></nav-component>
+                        <div class=posts>
                     <div class="posts-container"></div>
+                    </div>
                     <sidebar-component 
                     
                     ></sidebar-component>
