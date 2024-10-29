02373e759749832bcfccee4c403c473e2dff5c60
kikipou <kta2122006@gmail.com>
2024-10-29 15:41:56 -0500

    feat: login form

--- public/src/components/form/form.ts
+++ public/src/components/form/form.ts
@@ -27,5 +27,32 @@ class RegisterForm extends HTMLElement {
     }
 }
 console.log (RegisterForm)
-customElements.define('register-input', RegisterForm);
-export default RegisterForm;
+customElements.define('register-form', RegisterForm);
+
+class LoginForm extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+            };
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="./form.css.css">
+                    <h1 class="title">Mudy</h1>
+                        <form id="login-form">
+                            <input id="email" placeholder="Email" required />
+                            <input id="password" placeholder="Password" required />
+                            <button type="submit" id="Log In">Add song</button>
+                        </form>
+            `;
+        }
+    }
+}
+console.log (LoginForm)
+customElements.define('login-form', LoginForm);
