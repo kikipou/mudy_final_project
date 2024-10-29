b3fa51764f9f8593e1a9f6d436abe08f60345570
kikipou <kta2122006@gmail.com>
2024-10-29 12:33:26 -0500

    fix: register form page

---
+++ public/src/components/form/form.ts
@@ -0,0 +1,31 @@
+class RegisterForm extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+            };
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="./form.css.css">
+                    <h1 class="title">Mudy</h1>
+                        <form id="register-form">
+                            <input id="name" placeholder="Name" required />
+                            <input id="fullname" placeholder="Fullname" required />
+                            <input id="username" placeholder="Username" required />
+                            <input id="email" placeholder="Email" required />
+                            <input id="password" placeholder="Password" required />
+                            <button type="submit" id="Sign Up">Add song</button>
+                        </form>
+            `;
+        }
+    }
+}
+console.log (RegisterForm)
+customElements.define('register-input', RegisterForm);
+export default RegisterForm;
