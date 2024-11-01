964c9ad2ee5a00923b90d9426cc421316ca4cccd
LauraBonillaa <laurasofiabonilla@gmail.com>
2024-11-01 16:22:49 -0500

    fix:direccion css dashboard

--- public/src/screens/dashboard/dashboard.ts
+++ public/src/screens/dashboard/dashboard.ts
@@ -30,7 +30,7 @@ class Dashboard extends HTMLElement {
     render() {
         if (this.shadowRoot) {
             this.shadowRoot.innerHTML = `
-                <link rel="stylesheet" href="../public/src/index.css">
+                <link rel="stylesheet" href="../public/src/screens/dashboard/dashboard.css">
                 <div class="body">
                     <nav-component class="nav"
                         explore="Explore" 
