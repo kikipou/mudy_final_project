f728991fdb08957c147165fbe98c27005ed8cc0d
kikipou <kta2122006@gmail.com>
2024-11-01 11:29:34 -0500

    fix: getAuth

--- public/src/indexAbuelo.ts
+++ public/src/indexAbuelo.ts
@@ -1,6 +1,6 @@
-import RegisterPage from './screens/register/register';
-import Dashboard from './screens/dashboard/dashboard';
-import LoginPage from './screens/login/login';
+import './screens/register/register';
+import './screens/dashboard/dashboard';
+import './screens/login/login';
 import { addObserver, appState } from './store';
 import { Screens } from './types/store';
 
@@ -13,6 +13,7 @@ class AppContainer extends HTMLElement {
 
 	connectedCallback() {
 		this.render();
+		console.log(appState)
 	}
 
 	render() {
